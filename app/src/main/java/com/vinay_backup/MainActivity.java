56d5652ad9d01f6674276714938e1842bd23f8b6
shrey k <shreykadia.royal@gmail.com>
2022-07-07 18:04:59 +0530

    relative layout

---
+++ app/src/main/java/com/vinay_backup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vinay_backup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //we will write our java code here
+    }
+}
